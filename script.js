ad9b08a1b73170e09952b6ac67a92a081945c0e7
Anat Benski Lipman <anat_benski@hotmail.com>
2018-08-06 19:03:26 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 <html>
 <head>
   <title> My Site </title>
-  <!-- Put an include here in Part II -->
-</head>
+ <script type="javascript">
+      document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+    </script></head>
 <body>
   <p id="text">Eventing in JavaScript makes things look sharp.</p>
     <script type="javascript">
