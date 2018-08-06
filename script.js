f656144c910d66952037b3c13b1f44ed46e8987b
Anat Benski Lipman <anat_benski@hotmail.com>
2018-08-06 19:03:34 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -7,8 +7,6 @@
     </script></head>
 <body>
   <p id="text">Eventing in JavaScript makes things look sharp.</p>
-    <script type="javascript">
-      document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
-    </script>
+   
   </body>
 </html>
